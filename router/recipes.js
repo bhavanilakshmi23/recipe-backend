6bc531654587f8c24ba78ebc6a26662328b13bbb
bhavani <127599832+bhavanilakshmi23@users.noreply.github.com>
2023-11-18 21:27:53 +0530

    Add files via upload

---
+++ router/recipes.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const router = express.Router()
+
+const mongoose = require('mongoose')
+const {dbUrl} = require('../dbConfig')
+const {RecipesModel} = require('../schema/Recipes')
+
+
+mongoose.connect(dbUrl)
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await RecipesModel.find({});
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Create a new recipe
+router.post("/", async (req, res) => {
+  const recipe = new RecipesModel({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    image: req.body.image,
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions,
+    imageUrl: req.body.imageUrl,
+    cookingTime: req.body.cookingTime,
+    
+  });
+  console.log(recipe);
+
+  try {
+    const result = await recipe.save();
+    res.status(201).json({
+      createdRecipe: {
+        name: result.name,
+        image: result.image,
+        ingredients: result.ingredients,
+        instructions: result.instructions,
+        _id: result._id,
+      },
+    });
+  } catch (err) {
+    // console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Get a recipe by ID
+router.get("/:recipeId", async (req, res) => {
+  try {
+    const result = await RecipesModel.findById(req.params.recipeId);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports =router 
